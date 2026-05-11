e7cf74fce1a94b242300ee7af3d65fdd0efab749
kngxfrd <amekudzikingsford014@gmail.com>
2026-05-11 10:35:50 +0000

    lastest

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,6 +3,7 @@ import { IoIosInformationCircleOutline } from "react-icons/io";
 
 function Banner() {
   return (
+    //img src={} alt=""/
     <div className="bg-[url('src/assets/bannerimg.png')] bg-cover bg-center relative w-full h-150 pt-12 mt-16">
       <div className="w-100 flex mt-50 pl-6 flex-col ">
         <div className="text-center">
